26ac5c00ded2fad94078485751ad8bf6b6d4483c
LN <LN@MacBook-Pro-de-Mboa.local>
2022-08-11 12:49:51 +0100

    oragnisation

---
+++ src/data/data.js
@@ -0,0 +1,34 @@
+const data = [
+  {
+    id: 1,
+    img: "ln.jpg",
+    name: "Leonel Ngoya",
+    text: "Je tiens à te remercier pour ce magnifique cadeau d'anniversaire/de naissance/de mariage/de départ à la retraite. J'ai été très touché de cette jolie attention de ta part. Tu as toujours su trouver le cadeau parfait. J'ai beaucoup de chance de t'avoir dans ma vie.",
+  },
+  {
+    id: 2,
+    img: "ln.jpg",
+    name: "Leonel Ngoya",
+    text: "Je tiens à te remercier pour ce magnifique cadeau d'anniversaire/de naissance/de mariage/de départ à la retraite. J'ai été très touché de cette jolie attention de ta part. Tu as toujours su trouver le cadeau parfait. J'ai beaucoup de chance de t'avoir dans ma vie.",
+  },
+  {
+    id: 3,
+    img: "ln.jpg",
+    name: "Leonel Ngoya",
+    text: "Je tiens à te remercier pour ce magnifique cadeau d'anniversaire/de naissance/de mariage/de départ à la retraite. J'ai été très touché de cette jolie attention de ta part. Tu as toujours su trouver le cadeau parfait. J'ai beaucoup de chance de t'avoir dans ma vie.",
+  },
+  {
+    id: 4,
+    img: "ln.jpg",
+    name: "Leonel Ngoya",
+    text: "Je tiens à te remercier pour ce magnifique cadeau d'anniversaire/de naissance/de mariage/de départ à la retraite. J'ai été très touché de cette jolie attention de ta part. Tu as toujours su trouver le cadeau parfait. J'ai beaucoup de chance de t'avoir dans ma vie.",
+  },
+  {
+    id: 5,
+    img: "ln.jpg",
+    name: "Leonel Ngoya",
+    text: "Je tiens à te remercier pour ce magnifique cadeau d'anniversaire/de naissance/de mariage/de départ à la retraite. J'ai été très touché de cette jolie attention de ta part. Tu as toujours su trouver le cadeau parfait. J'ai beaucoup de chance de t'avoir dans ma vie.",
+  },
+];
+
+export default data;
