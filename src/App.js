b20f946f47474243d4f18845c89a2344cfedf4e2
ln-dev7 <leonelngoya@gmail.com>
2022-08-12 02:21:17 +0100

    finish tuto

--- src/App.js
+++ src/App.js
@@ -1,9 +1,31 @@
-
+import data from "./data/data.js";
+import { Splide, SplideSlide } from "@splidejs/react-splide";
+import "@splidejs/react-splide/css";
 
 function App() {
   return (
     <div className="container">
-      <h1>Tuto React Splide</h1>
+      <h1>Tutoriel React Splide</h1>
+      <div className="cards">
+        <Splide aria-label="Testimonials" options={{
+          type: "loop",
+          fixedWidth: "300px",
+          focus: "center",
+          isNavigation: true,
+          autoplay: true,
+          //perPage: 3,
+        }}>
+          {data.map((item) => (
+            <SplideSlide key={item.id}>
+              <div className="card">
+                <img src={item.img} alt="img" />
+                <h2>{item.name}</h2>
+                <p>{item.text}</p>
+              </div>
+            </SplideSlide>
+          ))}
+        </Splide>
+      </div>
     </div>
   );
 }
--- src/data/data.js
+++ src/data/data.js
@@ -29,6 +29,12 @@ const data = [
     name: "Leonel Ngoya",
     text: "Je tiens à te remercier pour ce magnifique cadeau d'anniversaire/de naissance/de mariage/de départ à la retraite. J'ai été très touché de cette jolie attention de ta part. Tu as toujours su trouver le cadeau parfait. J'ai beaucoup de chance de t'avoir dans ma vie.",
   },
+  {
+    id: 6,
+    img: "ln.jpg",
+    name: "Leonel Ngoya",
+    text: "Je tiens à te remercier pour ce magnifique cadeau d'anniversaire/de naissance/de mariage/de départ à la retraite. J'ai été très touché de cette jolie attention de ta part. Tu as toujours su trouver le cadeau parfait. J'ai beaucoup de chance de t'avoir dans ma vie.",
+  },
 ];
 
 export default data;
